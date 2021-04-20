0ac55edff6396500e57d49029393177d0568615e
akshatshah229 <71038035+akshatshah229@users.noreply.github.com>
2021-04-20 02:29:10 -0500

    aki

--- app/src/main/java/com/example/mavshop/categories.java
+++ app/src/main/java/com/example/mavshop/categories.java
@@ -1,23 +1,48 @@
 package com.example.mavshop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.Navigation.findNavController;
 
 public class categories extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_menu);
+        bottomNavigationView.setSelectedItemId(R.id.categories);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.categories:
+                        return true;
+                    case R.id.add_product:
+                        startActivity(new Intent(getApplicationContext(), add_product.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.view_profile:
+                        startActivity(new Intent(getApplicationContext(), view_profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
 
     public void OpenFurniture (View view) {
         Intent intent1 = new Intent(this, Furniture.class);
@@ -48,6 +73,7 @@ public class categories extends AppCompatActivity {
         Intent intent1 = new Intent(this, Miscellaneous.class);
         startActivity(intent1);
     }
+
 
 
 
